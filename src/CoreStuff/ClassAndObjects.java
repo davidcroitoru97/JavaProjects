ffdc918da5987e87f2e613c93f2c0946b19c1818
RamseyCRV <robert.crv@icloud.com>
2021-03-21 13:56:58 +0200

    Task1

---
+++ src/CoreStuff/ClassAndObjects.java
@@ -0,0 +1,13 @@
+package CoreStuff;
+
+/**
+ * Aici joacate cu clasele si obiectele aferentei ei
+ */
+public class ClassAndObjects {
+
+    /*
+    Fa te rog o metoda ca sa returneze suma celor 2 variabile
+     */
+    private int varsta =5;
+    private int varsta2 = 3;
+}
