db7f62016815cc0d591d01f0b57a58fb7be053f7
RamseyCRV <robert.crv@icloud.com>
2021-03-21 14:44:06 +0200

    Chapter 1

---
+++ src/Introduction/Objects.java
@@ -0,0 +1,14 @@
+package Introduction;
+
+// TODO-1: Citeste despre ce seminifica o clasa si ce este un obiect
+// TODO-2: Citeste ce este un constructor si ce este o instanta
+public class Objects {
+
+    // TODO-3: Creiaza un constructor fara parametrii pentru aceasta clasa
+
+    // TODO-4: Creiaza un constructor cu parametrii(nume, prenume, varsta) pentru aceasta clasa
+
+    // TODO-5: Creiaza o metoda care insumeaza 2 variabile de tip int
+
+    // TODO-6: Creiaza o metoda toString care sa afiseze valorile constructorului cu parametrii
+}
---
+++ src/Introduction/introductionMain.java
@@ -0,0 +1,20 @@
+package Introduction;
+
+public class introductionMain {
+
+    // TODO: Aici vei apela toate metodele din clasele pe care le vei face in pachetul "Introduction"
+    public static void main(String []args){
+
+        //TODO-7: Apeleaza constructorii si metodele din clasa Objects aici
+
+        /*
+         TODO-7: Citeste si scrie/invata despre ce este
+            - o clasa
+            - un obiect
+            - un constructor
+            - specificatorii de acces
+            - ce este : int, float, double, char, String
+            - ce este o instanta
+         */
+    }
+}
