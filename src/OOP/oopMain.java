5a4b53c5329c46704ef922cac8a0c2abefabdc36
RamseyCRV <robert.crv@icloud.com>
2021-03-25 20:49:25 +0200

    TODO-Important

--- src/OOP/oopMain.java
+++ src/OOP/oopMain.java
@@ -15,4 +15,21 @@ public class oopMain {
     public static void main(String []args){
 
     }
+
+    /* TODO-IMPORTANT : Ti-am facut un pachet care se numeste mostenire, in acel pachet dupa ce o sa citesti vreau ca
+    *   dupa ce citesti despre mostenire, sa iti faci 2, 3 clase pe care o sa le apelezi in acest main.
+    *   In aceste clase vreau sa ai o clasa principala si mai multe secundare care sa mosteneasca caracteristici
+    *   de la clasa mama. In engleza, termenul de mostenire este cunoscut ca  (inheritance).
+    *   Prin topicul asta o sa vreau  sa folosesti si conceptul de incapsulare pentru ce o sa faci.
+    *   Aici o sa ai un exercitiu, vreau sa te gandesti la o clasa mama (ex: Masina) care ar avea ca atribute
+    *   niste caracteristici comune, gen (numar roti, capacitate cilindrica, motorizare etc.). Clasele secundare care
+    *   pot fi de tipul (Audi, BMW, Dacia etc.) o sa mosteneasca acele caracteristici si in plus o sa detina fiecare
+    *   niste caracteristici proprii (ex: Audi o sa contina niste caracteristici in plus de tipul "numar geamuri, an fabricatie etc"*
+    *   Ca intr-un final Clasa Audi sa detina atributele mostenite (numar roti, capacitate cilindrica etc) + cele proprii (numar geamurir, an fabricatie)
+    *   Acesta este un exemplu, tu gandeste te la un alt exemplu. Este foarte bine sa antrenezi aceasta gandire, pentru ca tot ce faci
+    *   este orientat spre obiect, adica creiezi obiecte. Exemplul asta iti ofera o gandire de ansamblu asupra cum ai putea sa pui problema
+    *   in anumite cirmustante. Vreau sa te joci foarte bine cu mostenirea ( si sa citesti despre "this" si "super") si vreau
+    *   sa incluzi toate conceptele invatate pana acum, si evident sa INVETI TEORIA SI SA TI-O NOTEZI, pentru ca este un topic
+    *   primordial la orice interviu de angajare, indiferent de limbajul de programare.
+    * */
 }
