e1a8ca73524c5170a1fd2d16f3aedcb893f3ddec
davidcroitoru <croitorudavid4@gmail.com>
2021-04-05 00:43:41 +0300

    pe mail am scris tot

---
+++ src/ClaseAbstracteSiInterfete/Animal.java
@@ -0,0 +1,9 @@
+package ClaseAbstracteSiInterfete;
+
+public class Animal {
+
+    public abstract void Sunet();
+    public void sunete(){
+        System.out.println("miau");
+    }
+}
---
+++ src/ClaseAbstracteSiInterfete/Cat.java
@@ -0,0 +1,8 @@
+package ClaseAbstracteSiInterfete;
+
+public class Cat extends Animal {
+    public void Sunet() {
+        System.out.printnl("Pisica face: mrrrr")
+    }
+
+}
