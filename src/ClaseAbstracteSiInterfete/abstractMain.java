cf0957cde24479088e730a1ff19945e9965ff623
davidcroitoru <croitorudavid4@gmail.com>
2021-04-08 21:00:54 +0300

    pe mail am scris tot

--- src/ClaseAbstracteSiInterfete/abstractMain.java
+++ src/ClaseAbstracteSiInterfete/abstractMain.java
@@ -10,8 +10,8 @@ public class abstractMain {
     public static void main(String []args){
         Cat cat = new Cat();
 
-        //TODO-4: Creiaza o interfata cu cateva metode pe care sa le apelezi intr-o clasa implementand interfata si furnizandui o implementare.
-        //TODO-5: Creiaza o clasa abstracta si aplica principiile OOP pentru ea, in acelasi timp citeste care sunt functionalitatile pe care le detine o clasa abstracta fata de una normala si joaca te cu ele in cod.
+        //Rezolvat Creiaza o interfata cu cateva metode pe care sa le apelezi intr-o clasa implementand interfata si furnizandui o implementare.
+        //Rezolvat Creiaza o clasa abstracta si aplica principiile OOP pentru ea, in acelasi timp citeste care sunt functionalitatile pe care le detine o clasa abstracta fata de una normala si joaca te cu ele in cod.
 
         // Audi IS-A PatternMasina
         //PatternMasina patternMasina = new Audi();
