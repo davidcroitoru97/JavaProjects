0acb9ad277db7b3702c72712e2028998299e030e
RamseyCRV <robert.crv@icloud.com>
2021-04-19 10:33:28 +0300

    evaluaiton_3 solved

--- src/Evaluation_3/InterfaceProiect.java
+++
@@ -1,9 +0,0 @@
-package Evaluation_3;
-
-public interface InterfaceProiect {
-
-    String creareEchipa(Angajati nume, ProjectManager people);
-    String alocareProiect(String creareEchipa,Proiect proiect);
-    String detaliiAngajati(Angajati angajat);
-
-}
