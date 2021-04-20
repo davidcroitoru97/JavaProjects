f94e800777ba02979a002b6e03a24c1e1ef7cf93
davidcroitoru <croitorudavid4@gmail.com>
2021-04-21 00:51:04 +0300

    finish

---
+++ src/ObjectClass/Definitii.md
@@ -0,0 +1,12 @@
+1.Clasa Object(ObjectClass) este in mod implicit clasa parinte a tuturo claselor din java.
+Iar daca o clasa nu extinde alta clasa aceasta este derivata indirect din Clasa Object
+2. 
+2.aStringBuilder
+Reprezinta o secventa de caractere mutabila 
+   2.bStringBuffer 
+   Reprezinta o secventa de caractere la care se poate insera la mijloc sau la final alt sir de caractere
+   2.c String Este doar un sir de caractere
+De acici putem intelege si diferentele dintre cele 3 String-uri
+4.Generics = inseamna tipuri parametrizate
+
+Acesta permite String,Intiger ... si tipurilor definite de utilizator sa fie un parametru pentru metode clase si interfete.
--- src/ObjectClass/objectMain.java
+++ src/ObjectClass/objectMain.java
@@ -1,6 +1,45 @@
 package ObjectClass;
-
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 public class objectMain {
+
+        public static void main(String[] argv)
+                throws Exception
+        {
+
+            // Am creat un stringBuilder
+            StringBuilder q
+                    = new StringBuilder();
+
+            q.append("David");
+
+            // am afisat Stringul
+            System.out.println("q = "
+                    + q.toString());
+
+            // Am creat un StringBuilder
+            StringBuilder q1
+                    = new StringBuilder("Alex");
+
+            // afisam stringul 1
+            System.out.println("q1 = "
+                    + q1.toString());
+            //ce am stric in string mutam in string2
+            StringBuilder q2
+                    = new StringBuilder(q.toString());
+
+            // afisam stringul 2
+            System.out.println("q2 = "
+                    + q2.toString());
+
+        //SringBuffer
+
+            StringBuffer a = new StringBuffer("David");
+            a.append(" Croitoru"); //Adaugam Croitoru
+            System.out.println(a);
+            a.append(" Falticeni"); //Mai adaugam Falticeni
+            System.out.println(a);
+        }
 
     //  ca sa navighezi in IntelliJ trebuie sa folosesti "cmd + click"
     //  apasa "cmd + click" pe clasa Object ca sa te duca la metodele sale
